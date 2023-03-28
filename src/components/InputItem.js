9e3499b6364b263092f64a52785a8e173b1a8fc9
stephenkati <stephenkatuli88@gmail.com>
2023-03-28 18:55:10 +0300

    create functon for entering todo items

---
+++ src/components/InputItem.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+const InputItem = ({ addItem }) => {
+  const [title, setTitle] = useState('');
+  const [error, setError] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addItem(title);
+      setTitle('');
+    } else {
+      setError('Please add an item!');
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add item to list..."
+        value={title}
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+      <span>{error}</span>
+    </form>
+  );
+};
+InputItem.propTypes = {
+  addItem: PropTypes.func.isRequired,
+};
+export default InputItem;
