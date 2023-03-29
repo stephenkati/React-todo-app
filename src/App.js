0a198df0708178e908ceabba743b7b1d4a00438c
stephenkati <stephenkatuli88@gmail.com>
2023-03-29 09:17:16 +0300

    add div

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Functionality from './components/TodoFunctionality';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Functionality />
-    </>
+    </div>
   );
 }
 
