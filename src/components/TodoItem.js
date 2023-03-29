60b898e54f60d1eb1df4289891851abe0a83d5a4
stephenkati <stephenkatuli88@gmail.com>
2023-03-29 09:18:16 +0300

    add div for style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,69 @@
 import PropTypes from 'prop-types';
+import { useState, useRef } from 'react';
+import '../App.css';
+import { FaTrash } from 'react-icons/fa';
+import { AiFillEdit } from 'react-icons/ai';
 
-const TodoItem = ({ itemProp, handleChange, deleteItem }) => (
-  <li>
-    <input
-      type="checkbox"
-      checked={itemProp.completed}
-      onChange={() => handleChange(itemProp.id)}
-    />
-    {itemProp.title}
-    <button type="button" onClick={() => deleteItem(itemProp.id)}>delete</button>
-  </li>
-);
+const TodoItem = ({
+  itemProp,
+  handleChange,
+  deleteItem,
+  updateItem,
+}) => {
+  const [editItem, setEditItem] = useState(false);
+  const editInput = useRef(null);
+
+  const handleEditItem = () => {
+    setEditItem(true);
+  };
+  const viewMode = {};
+  const editMode = {};
+  if (editItem) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+  const handleUpdatedDone = (e) => {
+    if (e.key === 'Enter') {
+      updateItem(editInput.current.value, itemProp.id);
+      setEditItem(false);
+    }
+  };
+  return (
+    <li>
+      <div className="todoItem" style={viewMode}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        {itemProp.title}
+        <div className="icons">
+          <button type="button" className="edit" onClick={handleEditItem}>
+            <AiFillEdit />
+          </button>
+          <button type="button" className="delete" onClick={() => deleteItem(itemProp.id)}>
+            <FaTrash />
+          </button>
+        </div>
+      </div>
+      <input
+        type="text"
+        ref={editInput}
+        defaultValue={itemProp.title}
+        onChange={(e) => updateItem(e.target.value, itemProp.id)}
+        onKeyDown={handleUpdatedDone}
+        style={editMode}
+      />
+    </li>
+  );
+};
 
 TodoItem.propTypes = {
   itemProp: PropTypes.arrayOf.isRequired,
   handleChange: PropTypes.func.isRequired,
   deleteItem: PropTypes.func.isRequired,
+  updateItem: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
